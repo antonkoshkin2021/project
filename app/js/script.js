81d3d9065e1d15ea23f9998f72d13a4c74e0f86c
Anton <example@gmail.com>
2021-12-08 11:56:45 +0300

    Практическая работа по созданию Функций

--- app/js/script.js
+++ app/js/script.js
@@ -12,31 +12,108 @@
 // Код возьмите из предыдущего домашнего задания
 
 
-let arr = {
-    movies: {},
-    name: '',
-};
+// let arr = {
+//     movies: {},
+//     name: '',
+// };
 
-const a = prompt('Ваше имя', '');
+// const a = prompt('Ваше имя', '');
 
-arr.name = a;
+// arr.name = a;
 
-for (let i = 0; i < 2; i++) {
-    const b = prompt('Какой фильм последний смотрели?', '');
-    const d = prompt('сколько фильмов посмотрели', '');
 
-    if (b != '' && d != '' && b != null && d != null && b.length < 50) {
-        arr.movies[b] = d;
-        arr.count = d;
-    } else {
-        console.log('Ты делаешь что-то не так...');
-        i--;
+
+// console.log(arr);
+
+
+
+// function createCounter() {
+//     let counter = 0;
+//     const myFunction = function() {
+//         counter = counter + 1;
+//         return counter;
+//     };
+//     return myFunction;
+// }
+// const increment = createCounter();
+// const c1 = increment();
+// console.log(increment);
+
+
+// let test = '12.8 23 pxsdfsdfsdf 23';
+// console.log(parseFloat(test));
+
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms;
+
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
+}
+// start();
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 }
 
-if (arr.count < 10) {
-    console.log('Чувак, почему такой маленький рейтинг?');
-}
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const b = prompt('Какой фильм последний смотрели?', '');
+        const d = prompt('сколько фильмов посмотрели', '');
 
-console.log(arr);
+        if (b != '' && d != '' && b != null && d != null && b.length < 50) {
+            personalMovieDB.movies[b] = d;
+            personalMovieDB.count = d;
+        } else {
+            console.log('Ты делаешь что-то не так...');
+            i--;
+        }
+
+    }
+}
+// rememberMyFilms();
+
+
+
+function showMyDB() {
+    if (personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('У вас нет прав!');
+    }
+}
+// showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+writeYourGenres();
+
+
+console.log(personalMovieDB);
